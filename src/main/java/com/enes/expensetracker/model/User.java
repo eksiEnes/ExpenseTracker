744569ba1b5801c5124e12beda1433c7d1de2fe7
Enes Ekşi <enes-eksi@hotmail.com>
2024-10-14 00:53:05 +0300

    update User.java

--- src/main/java/com/enes/expensetracker/model/User.java
+++ src/main/java/com/enes/expensetracker/model/User.java
@@ -11,22 +11,29 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
-@Builder
 @Table(name = "users")
 public class User extends BaseEntity {
 
     @Id
+    @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
 
 
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Transaction> transactions;
 
 }
