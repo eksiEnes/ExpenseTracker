7c221be31d92fb5033e833e70fabb8ebe845b64a
Enes Ekşi <enes-eksi@hotmail.com>
2024-10-14 00:52:47 +0300

    update Transaction.java

--- src/main/java/com/enes/expensetracker/model/Transaction.java
+++ src/main/java/com/enes/expensetracker/model/Transaction.java
@@ -2,28 +2,29 @@ package com.enes.expensetracker.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Getter
 @Setter
 @Table(name = "transactions")
 public class Transaction extends BaseEntity {
 
     @Id
+    @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name="description")
     private String description;
+
+    @Column(name="amount")
     private BigDecimal amount; // Harcama miktarı
 
     // Kullanıcı ile ilişkilendirilen foreign key
-    @ManyToOne
+    @ManyToOne()
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
