2a6ceea9f81aff8aa9409467cb1989bc591b6cb5
Enes Ekşi <enes-eksi@hotmail.com>
2024-10-14 01:09:03 +0300

    update TransactionServiceImpl

--- src/main/java/com/enes/expensetracker/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/enes/expensetracker/service/impl/TransactionServiceImpl.java
@@ -1,11 +1,10 @@
 package com.enes.expensetracker.service.impl;
 
 import com.enes.expensetracker.model.Transaction;
-import com.enes.expensetracker.model.dto.request.Transaction.TransactionRequest;
-import com.enes.expensetracker.model.dto.request.Transaction.TransactionResponse;
+import com.enes.expensetracker.model.dto.request.transaction.TransactionRequest;
+import com.enes.expensetracker.model.dto.response.transaction.TransactionResponse;
 import com.enes.expensetracker.repository.TransactionRepository;
 import com.enes.expensetracker.service.TransactionService;
-import com.enes.expensetracker.service.UserService;
 import com.enes.expensetracker.service.business.TransactionBusinessRules;
 import com.enes.expensetracker.service.mapper.TransactionMapper;
 import lombok.RequiredArgsConstructor;
@@ -20,23 +19,21 @@ public class TransactionServiceImpl implements TransactionService {
 
     private final TransactionRepository transactionRepository;
     private final TransactionBusinessRules transactionBusinessRules;
-    private final TransactionMapper transactionMapper;
 
 
     @Override
     public List<TransactionResponse> findAll() {
         return  transactionRepository.findAll()
                 .stream()
-                .map(transactionMapper::fromTransaction)
+                .map(TransactionMapper.INSTANCE::fromTransaction)
                 .collect(Collectors.toList());
     }
-    // TODO BU FONKSİYON KONTROL EDİLCEK TRANSACTION OLUŞTURURKEN USER ID KISMI SIKINTILI MAP STRUCT KULLANMAK ZORUNDA KALICAM GIBI
     @Override
     public TransactionResponse createTransaction(TransactionRequest transactionRequest) {
-        transactionBusinessRules.validateTransactionRequest(transactionRequest);
+        transactionBusinessRules.isUserIdExist(transactionRequest.userid());
         Transaction transaction = transactionBusinessRules.prepareTransactionForCreate(transactionRequest);
         transactionRepository.save(transaction);
-        return transactionMapper.fromTransaction(transaction);
+        return TransactionMapper.INSTANCE.fromTransaction(transaction);
     }
 
     @Override
