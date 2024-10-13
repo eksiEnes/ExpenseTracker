683078423fee2211a24ffbaee8a7f2e2ddadf723
Enes Ekşi <enes-eksi@hotmail.com>
2024-10-14 01:11:12 +0300

    update TransactionBusinessRules

--- src/main/java/com/enes/expensetracker/service/business/TransactionBusinessRules.java
+++ src/main/java/com/enes/expensetracker/service/business/TransactionBusinessRules.java
@@ -2,40 +2,35 @@ package com.enes.expensetracker.service.business;
 
 import com.enes.expensetracker.exception.type.BusinessException;
 import com.enes.expensetracker.model.Transaction;
-import com.enes.expensetracker.model.User;
-import com.enes.expensetracker.model.dto.request.Transaction.TransactionRequest;
+import com.enes.expensetracker.model.dto.request.transaction.TransactionRequest;
 import com.enes.expensetracker.repository.TransactionRepository;
 import com.enes.expensetracker.service.UserService;
+import com.enes.expensetracker.service.mapper.TransactionMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionBusinessRules {
 
     private final TransactionRepository transactionRepository;
-    private final UserService userService;
+    private final UserService userService; // UserService hâlâ burada, ancak sadece UserResponse alacağız
 
-    // TransactionRequest üzerindeki iş kuralları doğrulama
-    public void validateTransactionRequest(TransactionRequest request) {
-        // Örnek: Transaction açıklaması boş olamaz vb.
-        if (request.Amount().compareTo(BigDecimal.ZERO) <= 0) {
-            throw new BusinessException("Transaction amount must be positive.");
+
+
+    public void isUserIdExist(long id){
+        if(userService.findById(id) == null){
+            throw new BusinessException(String.format("Transaction not found with id: " + id));
         }
     }
-
     // Transaction oluştururken gerekli iş mantığı
     public Transaction prepareTransactionForCreate(TransactionRequest request) {
-        // Kullanıcıyı e-posta ile bul ve transaction'a ata
-          var user = userService.findById(request.userid());
 
-        return Transaction.builder()
-                .description(request.Description())
-                .amount(request.Amount())
-                //.user()
-                .build();
+        userService.findById(request.userid());
+        Transaction transaction = TransactionMapper.INSTANCE.toTransaction(request);
+        transactionRepository.save(transaction);
+        return transaction;
     }
 
     // Transaction mevcut mu kontrolü
