b22c60e7cd36936dda18188763618fcd7b64a801
Enes Ekşi <enes-eksi@hotmail.com>
2024-10-13 09:15:05 +0300

    create user controller

---
+++ src/main/java/com/enes/expensetracker/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.enes.expensetracker.controller;
+
+import com.enes.expensetracker.model.dto.request.User.UserRequest;
+import com.enes.expensetracker.model.dto.request.User.UserResponse;
+import com.enes.expensetracker.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(
+            @RequestBody @Valid UserRequest request
+    ){
+        return ResponseEntity.ok(userService.createUser(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> findAllUsers() {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @GetMapping("/{user-id}")
+    public ResponseEntity<UserResponse> findUserByIdl(
+            @PathVariable("user-id") Long userId) {
+        return ResponseEntity.ok(userService.findById(userId));
+    }
+
+    @PutMapping("/{user-id}")
+    public ResponseEntity<UserResponse> updateUser(
+            @PathVariable("user-id") Long userId,
+            @RequestBody @Valid UserRequest request
+    ) {
+        return ResponseEntity.ok(userService.updateUser(userId, request));
+    }
+
+    @DeleteMapping("/{user-id}")
+    public ResponseEntity<Void> deleteUser(
+            @PathVariable("user-id") Long userId
+    ) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build(); // 204 No Content döner, başarılı silme işleminde içerik dönmez
+    }
+
+}
